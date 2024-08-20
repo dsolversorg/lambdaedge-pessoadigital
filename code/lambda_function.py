f60e40f4269202df187ab4142d3f6c872cdb34b6
consultitds <consultoriati@digitalsolvers.com>
2024-08-19 23:10:48 -0300

    Update lambda_function.py

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -27,6 +27,7 @@ def lambda_handler(event, context):
                 'antiTabagismo',
                 'ansiedade',
                 'ansiedade2',
+                'ansiedade-copilot',
             ]
     
     # Dicionário com as regras de redirecionamento
