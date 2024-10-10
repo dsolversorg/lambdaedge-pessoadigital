beadd756bc1ff05d44a7b5d7fa92c87342e5b880
Fabio Quatrochi <80180499+Quatrochifh@users.noreply.github.com>
2024-10-10 20:56:28 -0300

    Update lambda_function.py

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -31,6 +31,7 @@ def lambda_handler(event, context):
                 'jbss',
                 'trainia-copilot-medicina',
                 'ovario-dev',
+                'colgate',
         
             ]
     
